f922d7da27adc60514c9c3ec8c9366eef7f927fb
ReshetovE <reshetovegorr@gmail.com>
2017-10-29 03:32:45 +0600

    change starting class, added checkstyle file in project

--- src/main/java/it/sevenbits/formatter/Formatter.java
+++ src/main/java/it/sevenbits/formatter/Formatter.java
@@ -7,11 +7,5 @@ package it.sevenbits.formatter;
  * Formatter implementation
  */
 public class Formatter {
-    /**
-     * Point to program
-     * @param args command line args, ignored
-     */
-    public static void main(final String[] args){
-        System.out.println("Format");
-    }
+
 }
---
+++ src/main/java/it/sevenbits/formatter/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Package containing Main.
+ */
+package it.sevenbits.formatter;
+/**
+ * Main implementation.
+ */
+public class Main {
+    /**
+     * Start point to program.
+     * @param args command line args, ignored.
+     */
+    public static void main(final String[] args){
+
+    }
+}
