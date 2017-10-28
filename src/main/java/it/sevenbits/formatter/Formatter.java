5021152633d224fb73362275eb4dfe22d1513881
ReshetovE <reshetovegorr@gmail.com>
2017-10-28 13:14:04 +0600

    create maven project, added checkstyle

---
+++ src/main/java/it/sevenbits/formatter/Formatter.java
@@ -0,0 +1,17 @@
+/**
+ * Package containing Formatter.
+ */
+package it.sevenbits.formatter;
+
+/**
+ * Formatter implementation
+ */
+public class Formatter {
+    /**
+     * Point to program
+     * @param args command line args, ignored
+     */
+    public static void main(final String[] args){
+
+    }
+}
