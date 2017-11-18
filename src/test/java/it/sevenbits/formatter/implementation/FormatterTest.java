3aa93082650cfcb9b71b4551df5132089bf0a762
ReshetovE <reshetovegorr@gmail.com>
2017-11-19 03:41:51 +0600

    del *.iml file

--- src/test/java/it/sevenbits/formatter/implementation/FormatterTest.java
+++ src/test/java/it/sevenbits/formatter/implementation/FormatterTest.java
@@ -11,10 +11,11 @@ public class FormatterTest {/*
     private IFormatter format = new Formatter();
     private StringWriter out = new StringWriter();
     private StringReader in = null;
+    private ILexer lexer = new Lexer(in);
     @Test
     public void TestBrackets() throws ReaderException, FormatterException, WriterException {
         in = new StringReader("fdfddfs;fdsf;fdsa;afd");
-        format.format(in, out);
+        format.format(lexer, out);
         String sample ="fdfddfs;\nfdsf;\nfdsa;\nafd";
         assertEquals("Error test of brackers", sample, out.getString());
     }
@@ -22,7 +23,7 @@ public class FormatterTest {/*
     @Test
     public void TestOpenBrace() throws ReaderException, FormatterException, WriterException {
         in = new StringReader("sfd{fsda{d;sf");
-        format.format(in, out);
+        format.format(lexer, out);
         String sample ="sfd{\n    fsda{\n        d;\n        sf";
         assertEquals("Error test of open brace", sample, out.getString());
     }
@@ -30,7 +31,7 @@ public class FormatterTest {/*
     @Test
     public void TestCloseBrace() throws ReaderException, FormatterException, WriterException {
         in = new StringReader("f{dfgdfg{dsf;}}");
-        format.format(in, out);
+        format.format(lexer, out);
         String sample ="f{\n    dfgdfg{\n        dsf;\n    }\n}\n";
         assertEquals("Error test of close brace", sample, out.getString());
     }*/
